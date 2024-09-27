471030d60cbe635587d148fc9125ff7d4161cdaa
Rachel Sollars <ms244197@falmouth.ac.uk>
2024-09-27 21:50:38 +0100

    Added text scrolling script

---
+++ Assets/Scripts/TextScrollingScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextScrollingScript : MonoBehaviour
+{
+    string inputText = "This is a text scrolling test. The text should be scrolling naturally. Test. Test. Test. Test. Test. Test. Test. Test.";
+
+    float letterClock = 0f;
+    int pos = 0;
+
+    public TextMeshProUGUI tmp;
+    public float scrollSpeed = 0.04f;
+
+    string buildUpString = "";
+
+    void Start()
+    {
+        for (int i = 0; i < inputText.Length; i++)
+        {
+            StartCoroutine(PrintInOrder(i, inputText[i]));
+        }
+    }
+
+    IEnumerator PrintInOrder(int i, char s)
+    {
+        yield return new WaitForSeconds(scrollSpeed * i);
+        buildUpString += s;
+        tmp.text = buildUpString;
+    }
+}
