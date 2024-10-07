21595b66ed7f20f82c251c9eb79697a223ee256b
RACHEL SOLLARS <MS244197@falmouth.ac.uk>
2024-10-07 10:05:23 +0100

    started wiggle script and deleted terrain folder

---
+++ Assets/Scripts/Animations/Wiggle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiggle : MonoBehaviour
+{
+    Transform tf;
+    Vector3 startPos;
+
+    private void Start()
+    {
+        startPos =  
+    }
+
+    bool wiggling = false;
+
+    void SetWiggling(bool b)
+    {
+        wiggling = b;
+    }
+
+    void Update()
+    {
+            
+    }
+}
