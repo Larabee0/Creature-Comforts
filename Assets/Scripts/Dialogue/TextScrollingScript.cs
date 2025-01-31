b5a91c28a6268d1f0aaa60b2e63a25b59bbadab2
RACHEL SOLLARS <MS244197@falmouth.ac.uk>
2025-01-31 09:35:48 +0000

    Fixed MD tagging in text scrolling

--- Assets/Scripts/Dialogue/TextScrollingScript.cs
+++ Assets/Scripts/Dialogue/TextScrollingScript.cs
@@ -21,38 +21,41 @@ public class TextScrollingScript : MonoBehaviour
 
     public void ScrollText(string name = "NO_NAME_GIVEN", string inputText = "NO_TEXT_GIVEN")
     { 
-        endMarker.enabled = false;
-        List<string> constructionParts = new List<string>();
-        string stringPart = "";
-        for (int i = 0; i < inputText.Length; i++)
+        endMarker.enabled = false; //turns off endmarker at the start of line scroll
+
+        List<string> constructionParts = new List<string>(); // list to hold all the parts of the line
+        string stringPart = ""; // this stores the part of the string that is being constructed
+        for (int i = 0; i < inputText.Length; i++) // for each character in the line
         {
-            if (inputText[i] == '<')
+            if (inputText[i] == '<' && i > 0) // if the current character is the start of a MD tag and not the first character
             {
-                constructionParts.Add(stringPart);
-                stringPart = "";
-                stringPart += inputText[i];
+                constructionParts.Add(stringPart); // add what you have made to construction parts
+                stringPart = ""; // reset string part
+                stringPart += inputText[i]; // add this character to string part
             }
-            else if (inputText[i] == '>')
+            else if (inputText[i] == '>') // if the current character is the end of a MD tag
             {
-                stringPart += inputText[i];
-                constructionParts.Add(stringPart);
-                stringPart = "";
+                stringPart += inputText[i]; // add the current character to string part
+                constructionParts.Add(stringPart); // add that you have made to construction parts
+                stringPart = ""; // reset string part
+                if (i == inputText.Length - 1)
+                    break;
             }
-            else
+            else // if current characte is not the start or end of a MD tag
             {
-                stringPart += inputText[i]; 
+                stringPart += inputText[i]; // add current character to string part
             }
-            if (i == inputText.Length - 1)
+            if (i == inputText.Length - 1) // if you have reached the end of the line
             {
-                constructionParts.Add(stringPart);
+                constructionParts.Add(stringPart); // add what you have made to construction parts
             }
         }
 
-        speaker.text = name;
-        buildUpString = "";
-        int strLen = inputText.Length;
-        int placeKeeper = 0;
-        for (int i = 0; i < constructionParts.Count; i++)
+        // speaker.text = name; // DEPRICATED
+        buildUpString = ""; // creates a place to store text as it is made
+        int strLen = inputText.Length; // find out length of line so that inputs can be unpaused
+        int placeKeeper = 0; // create a value to keep the place in line
+        for (int i = 0; i < constructionParts.Count; i++) // for each chunk of dialogue and MD tag
         {
             if (constructionParts[i][0] == '<')
             {
